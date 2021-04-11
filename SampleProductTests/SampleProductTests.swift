dfc74eaff7f58225d5535540ccb6db7e57b3a974
hidec-7 <hiedematoaho0730127@gmail.com>
2021-04-11 20:13:20 +0900

    修正

--- SampleProductTests/SampleProductTests.swift
+++ SampleProductTests/SampleProductTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import sampleProduct
 
-class sampleProductTests: XCTestCase {
+class SampleProductTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
